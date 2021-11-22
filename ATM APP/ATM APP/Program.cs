04e7256a02735e2ecb599bc9d01ff65240f54da1
Serkan <94819174+Serkangur@users.noreply.github.com>
2021-11-22 16:00:04 +0300

    ATM app
    c# atm app

---
+++ ATM APP/ATM APP/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bakiye = 1000;
+
+            Console.WriteLine("atm ye hoşgeldiniz abe");
+            Console.WriteLine("yapmak istediğiniz işlemi seçiniz");
+
+            string secim = Console.ReadLine();
+
+            if (secim == "1")
+            {
+                Console.WriteLine("toplam bakiyeniz= " + bakiye);
+                Console.ReadLine();
+            }
+
+            else if (secim == "2")
+            {
+
+
+                Console.WriteLine("çekmek istediğiniz değeri giriniz");
+                int cekilecek_bakiye = Convert.ToInt32(Console.ReadLine());
+
+                if (cekilecek_bakiye < bakiye)
+                {
+                    Console.WriteLine("kalan tutuarınız= " + (bakiye - cekilecek_bakiye));
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("yeterli bakiyen yok tekrar dene gardaşım");
+                    Console.ReadLine();
+                }
+
+
+
+
+
+            }
+            else if (secim == "3")
+            {
+                Console.WriteLine("yatırmak istediğiniz tutarı giriniz");
+
+                int yatir = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("yeni tuturanız=" + (bakiye + yatir));
+                Console.ReadLine();
+            }
+
+            else if (secim == "q")
+            {
+                Console.WriteLine("atm'den çıkış yapılıyor");
+                Console.WriteLine("Allaha emenatsin");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("yanlış tuşlama yaptınız adam gibi tuşlayın");
+                Console.ReadLine();
+            }
+        }
+    }
+}
